64cba7d67e5ea2b13cc7ad41f1a44798405e291c
Aakash Makwana <makaakash2108@gmail.com>
2025-04-11 11:08:51 -0400

    IPL_2.py

--- IPL.py
+++ IPL.py
@@ -1,3 +1,11 @@
+
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/makaakash2108/IPL25_streamlit_app.git
+git push -u origin main
+
 {
  "cells": [
   {
